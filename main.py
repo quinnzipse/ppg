352de7b2b6857f8ed9410812b8033dc6a16f63ca
Quinn Zipse <qzipse@outlook.com>
2021-09-04 00:12:46 -0500

    Learned a lot, time to go a different direction
    Signed-off-by: Quinn Zipse <qzipse@outlook.com>

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 import numpy
 import cv2
-import matplotlib.pyplot as plt
+import time
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
 
 
-def show_histogram(input_image):
-    color = ('b', 'g', 'r')
-    for i, col in enumerate(color):
-        histogram = cv2.calcHist([input_image], [i], None, [256], [0, 256])
-        plt.plot(histogram, col)
-        plt.xlim([0, 256])
-    plt.show()
+fig, axs = plt.subplots(ncols=1, nrows=2)
+ax0 = axs[0]
+ax1 = axs[1]
+capture = cv2.VideoCapture(0)
 
+def animate(nothing):
+        success, img = capture.read()
+        data = [0, 256]
+        for i in range(0, 200):
+            for x in range(0, 200):
+                data.append(img[x, i, 2])
+        ax1.cla()
+        ax1.imshow(img)
+        ax0.cla()
+        ax0.hist(data, bins = 32, edgecolor="black", log=True)
 
 if __name__ == '__main__':
-    capture = cv2.VideoCapture(0)
-    success, image = capture.read()
-    cv2.imshow("Feed", image)
-
-    while cv2.getWindowProperty("Feed", 0) >= 0:
-        success, image = capture.read()
-
-        if success:
-            b = image[:, :, 0]
-            g = image[:, :, 1]
-            r = image[:, :, 2]
-
-            show_histogram(image)
-
-            cv2.imshow("Red Feed", r)
-            cv2.imshow("Green Feed", g)
-            cv2.imshow("Blue Feed", b)
-
-        cv2.waitKey(1)
-
+    _animation = animation.FuncAnimation(fig, animate, interval=50)
+    
+    plt.tight_layout()
+    plt.show()
     cv2.destroyAllWindows()
+
     capture.release()
