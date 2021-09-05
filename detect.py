11e4a8fbe18a6b08164475588153d4d79f34f96f
Quinn Zipse <qzipse@outlook.com>
2021-09-04 23:51:21 -0500

    Got face recognition to work, working on processing forehead data
    Signed-off-by: Quinn Zipse <qzipse@outlook.com>

--- detect.py
+++ detect.py
@@ -1,6 +1,32 @@
 import numpy as np
 import cv2 as cv
 import time
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvas
+
+fig = Figure()
+canvas = FigureCanvas(fig)
+axis = fig.subplots()
+axis.set_title("Red Histogram")
+
+
+def getRedHistogram(input_frame):
+    # blurred_img = cv.blur(input_frame, (2, 2))
+    # preprocessed = cv.convertScaleAbs(input_frame, alpha=1.1, beta=0)
+    red_frame = input_frame[:, :, 2]
+    red_histogram = cv.calcHist([red_frame], [0], None, [256], [0, 256])
+
+    axis.cla()
+    axis.plot(red_histogram)
+    axis.set_xlim([0, 256])
+    canvas.draw()
+
+    red_histogram_img = np.array(canvas.renderer.buffer_rgba())
+
+    cv.imshow('Red Histogram', red_histogram_img)
+    cv.imshow('Histogram Src', red_frame)
+
 
 if __name__ == '__main__':
     face_cascade = cv.CascadeClassifier('frontal_face.xml')
@@ -11,8 +37,10 @@ if __name__ == '__main__':
         exit()
 
     detected_duration = 0
-    detected_at = (0,0,0,0)
-    detected_at_threshold = 7
+    detected_at = (0, 0)
+    detected_at_threshold = 10
+    max_detected_duration = 0
+    extra_windows_open = False
 
     while True:
         successful, frame = capture.read()
@@ -25,31 +53,59 @@ if __name__ == '__main__':
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         faces = (), ()
-        threshold = 10
+        threshold = 5
         while len(faces) > 1:
             faces = face_cascade.detectMultiScale(gray, 1.3, threshold)
             threshold += 1
 
-        for (x,y,w,h) in faces:
-            cv.rectangle(gray, (x,y), (x+w,y+h), (255,255,0),2)
-            same_face = abs(detected_at[0] - x) < detected_at_threshold and abs(detected_at[1] - y) < detected_at_threshold
+        if len(faces) > 0:
+            x, y, w, h = faces[0]
+
+            center_x = int(x + (w / 2))
+            center_y = int(y + (h / 2))
+            cv.rectangle(frame, (center_x-1, center_y-1),
+                         (center_x+1, center_y+1), (0, 0, 255), 2)
+
+            subframe_start_x = int(x + (w*.3))
+            subframe_start_y = int(y + 5)
+            subframe_end_x = int(x+(w*.7))
+            subframe_end_y = int(y+(h/5))
+            cv.rectangle(frame, (subframe_start_x, subframe_start_y),
+                         (subframe_end_x, subframe_end_y), (0, 0, 0), 2)
+
+            same_face = abs(detected_at[0] - center_x) < detected_at_threshold and abs(
+                detected_at[1] - center_y) < detected_at_threshold
+
             if same_face:
                 detected_duration += 1
             else:
+                max_detected_duration = max(
+                    detected_duration, max_detected_duration)
+                print(max_detected_duration)
                 detected_duration = 0
-            
-            detected_at = (x,y,w,h)
-            center_x = int(x + (w / 2))
-            center_y = int(y + (h / 2))
-            radius = int( (w+h) /4)
-        
-            cv.circle(gray, (center_x, center_y), radius, (50 - detected_duration,detected_duration - 50,0),2)
-        
-        cv.imshow('detect_faces', gray)
-        print(detected_duration)
+            detected_at = (center_x, center_y)
+
+            radius = int((w+h) / 4)
+            red = 255 - (detected_duration * 6)
+            green = (detected_duration * 4) - 40
+            if green < 255:
+                cv.circle(frame, (center_x, center_y),
+                          radius, (0, green, red), 2)
+
+                if extra_windows_open:
+                    cv.destroyWindow('Red Histogram')
+                    cv.destroyWindow('Histogram Src')
+                    extra_windows_open = False
+            else:
+                sub_img = frame[
+                    subframe_start_y:subframe_end_y, subframe_start_x:subframe_end_x, :]
+                getRedHistogram(sub_img)
+                extra_windows_open = True
+
+        cv.imshow('detect_faces', frame)
 
         if cv.waitKey(30) == ord('q'):
             break
-        
+
     cv.destroyAllWindows()
     capture.release()
